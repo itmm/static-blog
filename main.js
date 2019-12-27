ee1eea5112f8a613aeb00fd0f9e3853e7443c6df
Timm Knape <timm@knp.de>
2019-12-27 15:15:27 +0100

    initial version

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	win.loadFile('index.html');
+};
+
+app.on('ready', createWindow);
+
